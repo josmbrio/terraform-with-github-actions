f0398840a3d0565d99486509c4b0023fc798e5eb
Jose <josmbrio@gmail.com>
2023-03-02 15:20:55 -0500

    Changed terraform code

--- main.tf
+++ main.tf
@@ -1,30 +1,8 @@
-data "aws_vpc" "current-default" {
-    default = true
-}
-
-resource "aws_security_group" "my-sg" {
-  name        = "my-sg-jbr"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = data.aws_vpc.current-default.id
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [data.aws_vpc.current-default.cidr_block]
-    ipv6_cidr_blocks = [data.aws_vpc.current-default.ipv6_cidr_block]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+resource "aws_vpc" "myvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
   tags = {
-    Name = "allow_tls"
+    Name = "myvpc"
   }
 }
