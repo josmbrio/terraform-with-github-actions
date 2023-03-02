911050e4be0dc17880068109dd0a3fa246650fa6
Jose <josmbrio@gmail.com>
2023-03-02 14:51:19 -0500

    Fixed syntax error calling a aws_vpc resource from data in main.tf

--- main.tf
+++ main.tf
@@ -5,15 +5,15 @@ data "aws_vpc" "current-default" {
 resource "aws_security_group" "my-sg" {
   name        = "my-sg-jbr"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.current-default.id
+  vpc_id      = data.aws_vpc.current-default.id
 
   ingress {
     description      = "TLS from VPC"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.current-default.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.current-default.ipv6_cidr_block]
+    cidr_blocks      = [data.aws_vpc.current-default.cidr_block]
+    ipv6_cidr_blocks = [data.aws_vpc.current-default.ipv6_cidr_block]
   }
 
   egress {
